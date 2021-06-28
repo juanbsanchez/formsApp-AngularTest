cae1a6f9d5a98f00709c53c3d7e5d78696bcc90f
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-28 08:49:08 +0200

    define routes and implements lazy load

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { BasicsComponent } from './basics/basics.component';
+import { DinamicsComponent } from './dinamics/dinamics.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsComponent,
+    DinamicsComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
