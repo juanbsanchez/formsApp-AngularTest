4b63e064cbba95547c45c241d953b322e909e83d
juanbsanchez <juanbsanchez1988@gmail.com>
2021-07-02 13:08:39 +0200

    form validations of dinamic.component of reactive part

--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Person {
+  name: string;
+  favs: Favs[];
+}
+
+interface Favs {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamics',
@@ -8,9 +19,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
+  newGame: string = '';
+
+  person: Person = {
+    name: 'John',
+    favs: [
+      {
+        id: 1,
+        name: 'Metal Gear'
+      },
+      {
+        id: 2,
+        name: 'Age of Empires'
+      }
+    ]
+  }
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [
+      Validators.required,
+      Validators.minLength(3)
+    ]]
+  }) 
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value)
+  }
+
+  addGame() {
+    
+  }
+
+  delete() {
+    
+  }
+
+  isValidField(field: string) {
+    return this.myForm.controls[field].errors &&
+      this.myForm.controls[field].touched;
   }
 
 }
