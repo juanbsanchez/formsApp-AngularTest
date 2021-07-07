1b2b42d61d889df4652a255b0a0280163b6eabf0
juanbsanchez <juanbsanchez1988@gmail.com>
2021-07-07 11:13:43 +0200

    input name vlidation with patterns

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // TODO temp
+  nameSurnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [
+      Validators.required,
+      Validators.pattern(this.nameSurnamePattern)
+    ]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  invalidInput(input: string) {
+    return this.myForm.get(input)?.invalid
+      && this.myForm.get(input)?.touched;
+  }
+
+  submitForm() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
   }
 
 }
